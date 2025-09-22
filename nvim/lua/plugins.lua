5563cd09cbce1495ed95b074ce47954d9098b944
Daniel Kasperovich <72548485+uproot9608@users.noreply.github.com>
2025-09-22 20:45:58 +0300

    Update nvim config

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,50 @@
+-- Set up lazy.nvim
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	-- Theme
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		config = function()
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	-- Treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup {
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python" },
+
+				highlight = {
+					enable = true,
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<Leader>ss",
+						node_incremental = "<Leader>si",
+						scope_incremental = false,
+						node_decremental = "<Leader>sd",
+					},
+				},
+			}
+		end,
+	},
+})
